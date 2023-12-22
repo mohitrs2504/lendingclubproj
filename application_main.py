639fc4807951ebfd0b7c4330d380b9f5ac4f10ef
Sumit Mittal <trendytech@Sumits-MacBook-Pro-2.local>
2023-12-22 19:19:55 +0530

    changes

--- application_main.py
+++ application_main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     job_run_env = sys.argv[1]
 
-    print("Creating Spark Session")
+    print("Creating Spark Session ")
 
     spark = Utils.get_spark_session(job_run_env)
 
